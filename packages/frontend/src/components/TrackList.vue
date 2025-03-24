a5a40091450fd65e5b48c3ef97270bb424f70743
theonag <theopointurier@gmail.com>
2025-03-24 15:05:06 +0100

    add array for css in trackList.vue

--- packages/frontend/src/components/TrackList.vue
+++ packages/frontend/src/components/TrackList.vue
@@ -2,20 +2,30 @@
 <template>
   <div class="tracks-section">
     <h3>{{ selectedPlaylist?.name || 'No Playlist Selected' }}</h3>
-    <ul class="track-list">
-      <li v-for="(item, index) in tracks" :key="item.track.id" class="track-item card"
-        @click="handlePlayTrack(item.track.uri)" :disabled="!isPlayerReady">
-        <button class="button-primary play-button">
-          Play
-        </button>
-        <span class="track-number">{{ index + 1 }}</span>
-        <img :src="item.track.album.images[2]?.url" alt="Album cover" class="track-album-cover" />
-        <div class="track-info">
-          <span class="track-name">{{ item.track.name }}</span>
-          <span class="artist-name">{{ item.track.artists[0].name }}</span>
-        </div>
-      </li>
-    </ul>
+    <table class="track-table">
+      <thead>
+        <tr>
+          <th class="number-title-bar">#</th>
+          <th></th> <!-- Colonne pour l'image -->
+          <th>Track</th>
+          <th>Artist</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in tracks" :key="item.track.id" class="track-item"
+          @click="handlePlayTrack(item.track.uri)" :class="{ disabled: !isPlayerReady }">
+          <td class="track-number-container">
+            <p class="track-number">{{ index + 1 }}</p>
+            <button class="button-primary play-button">Play</button>
+          </td>
+          <td>
+            <img :src="item.track.album.images[2]?.url" alt="Album cover" class="track-album-cover" />
+          </td>
+          <td class="track-name">{{ item.track.name }}</td>
+          <td class="artist-name">{{ item.track.artists[0].name }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -94,13 +104,10 @@ export default defineComponent({
   width: 100%;
   grid-column: 2 / 3;
   scrollbar-width: thin;
-  /* For Firefox */
   scrollbar-color: var(--spotify-white) transparent;
-  /* For Firefox */
 
   &::-webkit-scrollbar {
     width: 8px;
-    /* For WebKit browsers */
   }
 
   &::-webkit-scrollbar-thumb {
@@ -116,30 +123,57 @@ export default defineComponent({
 
   &:not(:hover)::-webkit-scrollbar-thumb {
     background-color: transparent;
-    /* Hide scrollbar thumb when not hovering */
   }
 }
 
-.track-list {
-  display: flex;
-  flex-direction: column;
+.track-table {
   width: 100%;
-  gap: 8px;
+  border-collapse: collapse;
+}
+
+.track-table thead {
+  position: sticky;
+  top: 0;
+  background-color: #1a1a1a;
+  color: var(--spotify-light-grey);
+  font-size: 14px;
+}
+
+.track-table th,
+.track-table td {
+  padding: 12px;
+  text-align: left;
 }
 
 .track-item {
-  display: flex;
+  border-bottom: 1px solid #2a2a2a;
+}
+
+.track-item:hover {
+  background-color: #2a2a2a;
+  cursor: pointer;
   width: 100%;
-  align-items: center;
-  gap: 12px;
-  padding: 12px;
+}
+
+.track-item.disabled {
+  opacity: 0.5;
+  pointer-events: none;
+}
+
+.track-table .number-title-bar {
+  text-align: center;
 }
 
 .track-number {
-  width: 24px;
   font-size: 14px;
   color: var(--spotify-light-grey);
   text-align: center;
+  opacity: 1;
+  transition: opacity 0.5s ease;
+}
+
+.track-number-container {
+  width: 5rem;
 }
 
 .track-album-cover {
@@ -147,10 +181,6 @@ export default defineComponent({
   height: 40px;
   border-radius: 4px;
   object-fit: cover;
-}
-
-.track-info {
-  flex: 1;
 }
 
 .track-name {
@@ -164,13 +194,22 @@ export default defineComponent({
 }
 
 .play-button {
-  padding: 6px 12px;
+  /* padding: 6px 12px; */
   font-size: 12px;
+  display: none;
+  opacity: 0;
+  transition: opacity 1s ease;
+  justify-self: center;
 }
 
-.track-item:hover {
-  background-color: #2a2a2a;
-  cursor: pointer;
+.track-item:hover .play-button {
+  display: block;
+  opacity: 1;
+}
+
+.track-item:hover .track-number {
+  display: none;
+  opacity: 0;
 }
 
 .track-item:hover .track-name {
