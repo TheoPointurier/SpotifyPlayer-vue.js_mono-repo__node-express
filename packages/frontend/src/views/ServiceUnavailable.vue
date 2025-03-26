2fe8d8e0c5e004a9c0f268ef77434061ad015ff4
theonag <theopointurier@gmail.com>
2025-03-26 17:26:03 +0100

    add message for not server yet

--- packages/frontend/src/views/ServiceUnavailable.vue
+++ packages/frontend/src/views/ServiceUnavailable.vue
@@ -2,9 +2,36 @@
   <div class="service-unavailable">
     <h1>Service Indisponible</h1>
     <p>Le serveur est temporairement hors ligne. Veuillez réessayer plus tard.</p>
-    <button @click="$router.push('/')">Retour à l’accueil</button>
+    <p v-if="message" class="temp-message">{{ message }}</p>
+    <button @click="navigateToHome">Retour à l’accueil</button>
   </div>
 </template>
+<script lang="ts" setup>
+
+import { useRouter } from 'vue-router';
+import { ref } from 'vue';
+
+const router = useRouter();
+const message = ref<string | null>(null);
+
+async function navigateToHome() {
+  try {
+
+    await router.push('/');
+
+  } catch (error) {
+    message.value = (error as Error).message;
+    console.error('Erreur lors de la navigation vers la page d’accueil:', error);
+
+    setTimeout(() => {
+      message.value = null;
+    }, 2000);
+  }
+
+}
+
+
+</script>
 
 <style scoped>
 .service-unavailable {
@@ -12,5 +39,15 @@
   padding: 2rem;
   color: var(--spotify-white);
   background-color: var(--spotify-dark-grey);
+}
+
+button {
+  margin-top: 1rem;
+}
+
+.temp-message {
+  color: var(--spotify-green);
+  font-weight: bold;
+  margin-bottom: 1rem;
 }
 </style>
