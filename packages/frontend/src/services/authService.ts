495e23f428a89de63c5406ef77b9f0aa47669b91
theonag <theopointurier@gmail.com>
2025-03-26 20:19:56 +0100

    add prefix for redirection auth

--- packages/frontend/src/services/authService.ts
+++ packages/frontend/src/services/authService.ts
@@ -17,7 +17,7 @@ export const fetchToken = async (): Promise<string> => {
     if ((error as Error).message.includes('Non authentifié')) {
       const customError = error as Error & { value?: string };
       customError.value = 'Veuillez vous connecter';
-      window.location.href = '/login';
+      window.location.href = '/spotify-app/login';
     }
     console.error('Erreur lors de la récupération du token:', error);
     throw error;
