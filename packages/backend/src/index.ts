23bbb2ab80f166f72adb90b8830d25d6e897e8ca
theonag <theopointurier@gmail.com>
2025-03-27 16:14:22 +0100

    delete doublon cors option

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -19,8 +19,8 @@ const corsOptions = {
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'], 
 };
-app.use(cors(corsOptions));
-app.options('*', cors());
+
+app.options('*', app.use(cors(corsOptions)));
 
 app.use(express.json());
 app.use(
